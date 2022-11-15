84f157b1c0e476aae80214bf5d1b390e6fb2832a
Midhun-666-slayer <114171730+Midhun-666-slayer@users.noreply.github.com>
2022-11-15 18:42:11 +0530

    Delete PROJECT DEVELOPMENT PHASE/SPRINT-I directory

--- PROJECT DEVELOPMENT PHASE/SPRINT-I/Flask/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request
-
-import os
-import cv2
-import operator
-from tensorflow.keras.models import load_model
-from werkzeug.utils import secure_filename
-
